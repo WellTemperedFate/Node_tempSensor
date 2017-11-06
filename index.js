686b2394e49c352c918d55c55161a5f16bfeb84f
Giovanni Allemeersch <giovanni.allemeersch@gmail.com>
2017-11-06 23:38:27 +0100

    another change another commit...

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var serialPort = require('serialport');
+/*var serialPort = require('serialport');
 var port = new serialPort('/dev/ttyAMA0', {
 	baudRate: 115200
-});
+});*/
 
-var dataToSend;
+//var dataToSend;
 
 /*setInterval(function(){ port.write("data\r\n",'utf8',function(){
 	var appel = port.read(1024);
@@ -28,17 +28,17 @@ serialPort.list(function (err,ports){
 	return stringus;
 }*/
 
-port.open(function(msg){
+/*port.open(function(msg){
 	if(msg){
 		console.log(msg.message);
 	}
-});
+});*/
 
 /*port.write("hallo\r\n",'utf8', function(){
 	console.log('data was sent');
 });*/
 
-port.on('open', function(msg){
+/**port.on('open', function(msg){
 	console.log('port Openened!');
 });
 
@@ -48,24 +48,28 @@ port.on('error', function(err){
 
 port.on('close', function() {
 	console.log('Closing...');
-});
+});*/
 
-port.on('data', function(data){
+/*port.on('data', function(data){
 	var length = data.length;
 	var stringus = "";
 	for(var index = 0; index<length; index+=1)
 		stringus += String.fromCharCode(data[index]);
 	dataToSend = stringus;
 	//console.log(WriteChars(data));
-});
+});*/
 
 /**
  * returns a string containing the temperature
  * @return {string}
  */
-exports.temperature = function()
+/*exports.temperature = function()
 {
 	return dataToSend;
+}*/
+
+exports.stringus = function(){
+	return "je moeder";
 }
 
 
